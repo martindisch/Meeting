7f30737e8ebb4f1bf74dba8cfe66fb7c940a6f70
Martin Disch <martindisch@gmail.com>
2015-06-06 10:21:22 +0200

    UI

--- Meeting/Form1.Designer.cs
+++ Meeting/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbData = new System.Windows.Forms.GroupBox();
+            this.lbSubjects = new System.Windows.Forms.ListBox();
+            this.bAddSubject = new System.Windows.Forms.Button();
+            this.lbParticipants = new System.Windows.Forms.ListBox();
+            this.bAddParticipants = new System.Windows.Forms.Button();
+            this.bSave = new System.Windows.Forms.Button();
+            this.bLoad = new System.Windows.Forms.Button();
+            this.gbMeetings = new System.Windows.Forms.GroupBox();
+            this.bGenerate = new System.Windows.Forms.Button();
+            this.lbMeetings = new System.Windows.Forms.ListBox();
+            this.gbData.SuspendLayout();
+            this.gbMeetings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbData
+            // 
+            this.gbData.Controls.Add(this.bLoad);
+            this.gbData.Controls.Add(this.bSave);
+            this.gbData.Controls.Add(this.bAddParticipants);
+            this.gbData.Controls.Add(this.lbParticipants);
+            this.gbData.Controls.Add(this.bAddSubject);
+            this.gbData.Controls.Add(this.lbSubjects);
+            this.gbData.Location = new System.Drawing.Point(13, 13);
+            this.gbData.Name = "gbData";
+            this.gbData.Size = new System.Drawing.Size(338, 588);
+            this.gbData.TabIndex = 0;
+            this.gbData.TabStop = false;
+            this.gbData.Text = "Daten";
+            // 
+            // lbSubjects
+            // 
+            this.lbSubjects.FormattingEnabled = true;
+            this.lbSubjects.ItemHeight = 16;
+            this.lbSubjects.Location = new System.Drawing.Point(7, 22);
+            this.lbSubjects.Name = "lbSubjects";
+            this.lbSubjects.Size = new System.Drawing.Size(325, 180);
+            this.lbSubjects.TabIndex = 0;
+            // 
+            // bAddSubject
+            // 
+            this.bAddSubject.Location = new System.Drawing.Point(165, 209);
+            this.bAddSubject.Name = "bAddSubject";
+            this.bAddSubject.Size = new System.Drawing.Size(167, 28);
+            this.bAddSubject.TabIndex = 1;
+            this.bAddSubject.Text = "Thema hinzufügen";
+            this.bAddSubject.UseVisualStyleBackColor = true;
+            // 
+            // lbParticipants
+            // 
+            this.lbParticipants.FormattingEnabled = true;
+            this.lbParticipants.ItemHeight = 16;
+            this.lbParticipants.Location = new System.Drawing.Point(7, 244);
+            this.lbParticipants.Name = "lbParticipants";
+            this.lbParticipants.Size = new System.Drawing.Size(325, 244);
+            this.lbParticipants.TabIndex = 2;
+            // 
+            // bAddParticipants
+            // 
+            this.bAddParticipants.Location = new System.Drawing.Point(166, 494);
+            this.bAddParticipants.Name = "bAddParticipants";
+            this.bAddParticipants.Size = new System.Drawing.Size(166, 28);
+            this.bAddParticipants.TabIndex = 3;
+            this.bAddParticipants.Text = "Teilnehmer hinzufügen";
+            this.bAddParticipants.UseVisualStyleBackColor = true;
+            // 
+            // bSave
+            // 
+            this.bSave.Location = new System.Drawing.Point(172, 554);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(160, 28);
+            this.bSave.TabIndex = 4;
+            this.bSave.Text = "Daten speichern";
+            this.bSave.UseVisualStyleBackColor = true;
+            // 
+            // bLoad
+            // 
+            this.bLoad.Location = new System.Drawing.Point(7, 554);
+            this.bLoad.Name = "bLoad";
+            this.bLoad.Size = new System.Drawing.Size(160, 28);
+            this.bLoad.TabIndex = 5;
+            this.bLoad.Text = "Daten laden";
+            this.bLoad.UseVisualStyleBackColor = true;
+            // 
+            // gbMeetings
+            // 
+            this.gbMeetings.Controls.Add(this.lbMeetings);
+            this.gbMeetings.Controls.Add(this.bGenerate);
+            this.gbMeetings.Location = new System.Drawing.Point(371, 13);
+            this.gbMeetings.Name = "gbMeetings";
+            this.gbMeetings.Size = new System.Drawing.Size(559, 588);
+            this.gbMeetings.TabIndex = 1;
+            this.gbMeetings.TabStop = false;
+            this.gbMeetings.Text = "Treffen";
+            // 
+            // bGenerate
+            // 
+            this.bGenerate.Location = new System.Drawing.Point(459, 554);
+            this.bGenerate.Name = "bGenerate";
+            this.bGenerate.Size = new System.Drawing.Size(94, 28);
+            this.bGenerate.TabIndex = 1;
+            this.bGenerate.Text = "Generieren";
+            this.bGenerate.UseVisualStyleBackColor = true;
+            // 
+            // lbMeetings
+            // 
+            this.lbMeetings.FormattingEnabled = true;
+            this.lbMeetings.ItemHeight = 16;
+            this.lbMeetings.Location = new System.Drawing.Point(7, 22);
+            this.lbMeetings.Name = "lbMeetings";
+            this.lbMeetings.Size = new System.Drawing.Size(546, 516);
+            this.lbMeetings.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(942, 613);
+            this.Controls.Add(this.gbMeetings);
+            this.Controls.Add(this.gbData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Meeting";
+            this.gbData.ResumeLayout(false);
+            this.gbMeetings.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbData;
+        private System.Windows.Forms.ListBox lbSubjects;
+        private System.Windows.Forms.Button bLoad;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Button bAddParticipants;
+        private System.Windows.Forms.ListBox lbParticipants;
+        private System.Windows.Forms.Button bAddSubject;
+        private System.Windows.Forms.GroupBox gbMeetings;
+        private System.Windows.Forms.ListBox lbMeetings;
+        private System.Windows.Forms.Button bGenerate;
     }
 }
 
