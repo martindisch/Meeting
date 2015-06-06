9266313001d11fc902e20d6a8956a1660584dd88
Martin Disch <martindisch@gmail.com>
2015-06-06 17:52:12 +0200

    ContextMenu with delete functionality

--- Meeting/MainForm.cs
+++ Meeting/MainForm.cs
@@ -24,6 +24,45 @@ namespace Meeting
             lbSubjects.DisplayMember = "Name";
             lbSubjects.ValueMember = "ID";
             NewNames = new List<String>();
+            ContextMenu cm = new ContextMenu();
+            cm.MenuItems.Add("Neues Treffen", new EventHandler(item_click));
+            cm.MenuItems.Add("Bearbeiten", new EventHandler(item_click));
+            cm.MenuItems.Add("Löschen", new EventHandler(item_click));
+            lbSubjects.ContextMenu = cm;
+            lbSubjects.MouseDown += lbSubjects_MouseDown;
+        }
+
+        void item_click(object sender, EventArgs e)
+        {
+            MenuItem clickedItem = sender as MenuItem;
+            switch (clickedItem.Text)
+            {
+                case "Neues Treffen":
+                    
+                    break;
+                case "Bearbeiten":
+
+                    break;
+                case "Löschen":
+                    if (lbSubjects.SelectedIndex != -1)
+                    {
+                        DS.Subjects.RemoveAt(lbSubjects.SelectedIndex);
+                    }
+                    break;
+            }
+        }
+
+        private void lbSubjects_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //select the item under the mouse pointer
+                lbSubjects.SelectedIndex = lbSubjects.IndexFromPoint(e.Location);
+                if (lbSubjects.SelectedIndex != -1)
+                {
+                    //listboxContextMenu.Show();
+                }
+            }
         }
 
         private void bAddSubject_Click(object sender, EventArgs e)
